b96d08d231ecd360a268e4a8ec6a83ac44d13d45
Ariel Putra <myakbararil@gmail.com>
2021-04-09 23:16:46 +0700

    updatev0.2

--- Assets/MyAssets/Ariel/Scripts/ArealCameraStandby.cs
+++ Assets/MyAssets/Ariel/Scripts/ArealCameraStandby.cs
@@ -8,82 +8,83 @@ public class ArealCameraStandby : MonoBehaviour
     [SerializeField] private Transform player;
 
     //menunujuk ke object tanda
-    [SerializeField] private Transform borderKanan;
-    [SerializeField] private Transform borderKiri;
+    [SerializeField] private Transform bKanan;
+    [SerializeField] private Transform bKiri;
 
     //menunjuk ke ujung gizmoz dari camera
-    [SerializeField] private Transform ujung1;
-    [SerializeField] private Transform ujung2;
+    [SerializeField] private Transform uKanan;
+    [SerializeField] private Transform uKiri;
 
     private float lebarLayar, tinggiLayar, jarakCam;
     private Vector2 offset;
     private Vector3 geser;
     private int pindahCam = 0;
-    private bool pindahDetect = false;
+    private bool pindahDetect = false, sudahPindah = false;
 
 
     private void Start()
     {
-        offset.Set((ujung2.position.x - borderKanan.position.x), (ujung2.position.y - borderKanan.position.y));
-
-        borderKiri.position.Set((ujung1.position.x + offset.x), (ujung1.position.y + offset.y), 0);
-
-        lebarLayar = ujung2.position.x - ujung1.position.x;
-
-        tinggiLayar = ujung2.position.y - ujung1.position.y;
-
-        jarakCam = ujung2.position.x - transform.position.x;
+        offset.Set((uKanan.position.x - bKanan.position.x), (uKanan.position.y - bKanan.position.y));
+        lebarLayar = uKanan.position.x - uKiri.position.x;
+        tinggiLayar = uKanan.position.y - uKiri.position.y;
+        jarakCam = uKanan.position.x - transform.position.x;
         geser = new Vector3();
     }
 
     private void Update()
     {
+        bKiri.position.Set((uKiri.position.x + offset.x), (uKiri.position.y + offset.y), 0);
         //jika player melewati border kanan
-        if ((player.position.x > borderKanan.position.x - 0.2) && (player.position.x < borderKanan.position.x + 0.2) && (pindahDetect == false))
+        if ((player.position.x > uKiri.position.x) && (player.position.x < bKiri.position.x) && (pindahDetect == false))
+        {
+            pindahCam = 1;
+            pindahDetect = true;
+        }
+        //Jika player melewati border kiri
+        else if ((player.position.x > bKanan.position.x) && (player.position.x < uKanan.position.x) && (pindahDetect == false))
         {
             pindahCam = 2;
         }
-        //Jika player melewati border kiri
-        else if ((player.position.x > borderKiri.position.x - 0.2) && (player.position.x < borderKiri.position.x + 0.2) && (pindahDetect == false))
-        {
-            pindahCam = 1;
-        } else
+        else
         {
             pindahDetect = false;
         }
-        
+
         switch (pindahCam)
         {
             case 1:
-                Debug.Log("case 1");
-                //mengeset border kiri sebagai border kanan yang baru
-                borderKanan.position.Set(borderKiri.position.x, borderKiri.position.y, 0);
-                //menggeser border kiri sebannyak lebarLayar ke kiri
-                borderKiri.position.Set((borderKiri.position.x - lebarLayar), borderKanan.position.y, 0);
-                transform.position.Set((transform.position.x - jarakCam * 2), transform.position.y, transform.position.z);
-                break;
+                {
+                    Debug.Log("case 1");
+                    uKiri.position.Set(uKiri.position.x - lebarLayar, uKiri.position.y, 0);
+                    uKanan.position.Set(uKanan.position.x - lebarLayar, uKanan.position.y, 0);
+                    bKanan.position.Set(bKanan.position.x - lebarLayar, bKanan.position.y, 0);
+                    bKiri.position.Set(bKiri.position.x - lebarLayar, bKiri.position.y, 0);
+                    geser.Set((transform.position.x - jarakCam * 2), transform.position.y, transform.position.z);
+                    transform.position = geser;
+                    pindahCam = 0;
+                    break;
+                }
             case 2:
-                Debug.Log("case 2");
-                //mengeset border kanan sebagai border kiri yang baru
-                borderKiri.position.Set(borderKanan.position.x, borderKiri.position.y, 0);
-                //menggeser border kanan sebanyak nilai dari lebarLayar
-                borderKanan.position.Set((borderKanan.position.x + lebarLayar), borderKanan.position.y, 0);
-                //transform.position.Set((transform.position.x + jarakCam * 2), transform.position.y, transform.position.z);
-                geser.Set((transform.position.x + jarakCam), transform.position.y, transform.position.z);
-                transform.position = geser;
-                pindahCam = 0;
-                pindahDetect = true;
-                break;
+                {
+                    Debug.Log("case 2");
+                    uKanan.position.Set(uKanan.position.x + lebarLayar, uKanan.position.y, 0);
+                    uKiri.position.Set(uKiri.position.x + lebarLayar, uKiri.position.y, 0);
+                    bKiri.position.Set(bKiri.position.x + lebarLayar, bKiri.position.y, 0);
+                    bKanan.position.Set((bKanan.position.x + lebarLayar), bKanan.position.y, 0);
+                    geser.Set((transform.position.x + jarakCam * 2), transform.position.y, transform.position.z);
+                    transform.position = geser;
+                    pindahCam = 0;
+                    break;
+                }
         }
-
-
     }
 
     private void FixedUpdate()
     {
-        Debug.Log(player.position.x == borderKanan.position.x);
-        Debug.Log((player.position.x > borderKanan.position.x - 2) && (player.position.x < borderKanan.position.x + 2));
+        Debug.Log(player.position.x == bKanan.position.x);
+        Debug.Log((player.position.x > bKanan.position.x - 2) && (player.position.x < bKanan.position.x + 2));
         Debug.Log(player.position.x);
-        Debug.Log(borderKanan.position.x);
+        Debug.Log(bKanan.position.x);
+        Debug.Log(bKiri.position);
     }
 }
